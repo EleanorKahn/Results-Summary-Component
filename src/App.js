5b92ca3897423e78b2e8ff1cd86a120e6722e380
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-04-19 10:12:08 -0700

    scaffolded as a React project

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+    Your Result
+    76
+    of 100
+
+    Great
+    You scored higher than 65% of the people who have taken these tests.
+    
+    Summary
+
+    Reaction
+    80 / 100
+
+    Memory
+    92 / 100
+
+    Verbal
+    61 / 100
+
+    Visual
+    72 / 100
+
+    Continue
+    </div>
+  
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="#">Eleanor</a>.
+  </div>
+    </div>
+  );
+}
+
+export default App;
