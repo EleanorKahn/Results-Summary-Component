15f531f5569a604f4516809aa1d5140ed876b915
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-04-19 11:00:53 -0700

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
-import logo from './logo.svg';
+import { Button } from "reactstrap";
+import "./css/style.css";
 import './css/App.css';
 
 function App() {
   return (
     <div className="App">
-      <div>
-    Your Result
-    76
-    of 100
+      <div className="wrapper">
+        <div className="result">
+            <h3>Your Result</h3>
+            <div className="totalScore">
+              <h1>76</h1>
+              <p>of 100</p>
+            </div>
+            <h2>Great</h2>
+            <p>You scored higher than 65% of the people who have taken these tests.</p>
+        </div>
+        <div className="summary">
+          <h3>Summary</h3>
+          <ul>
+            <li>Reaction
+            80 / 100</li>
 
-    Great
-    You scored higher than 65% of the people who have taken these tests.
-    
-    Summary
+            <li>Memory
+            92 / 100</li>
 
-    Reaction
-    80 / 100
+            <li>Verbal
+            61 / 100</li>
 
-    Memory
-    92 / 100
-
-    Verbal
-    61 / 100
-
-    Visual
-    72 / 100
-
-    Continue
-    </div>
+            <li>Visual
+            72 / 100</li>
+          </ul>
+          <Button>
+            Continue
+          </Button>
+        </div>
+      </div>
   
-  <div class="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Eleanor</a>.
-  </div>
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Eleanor</a>.
+      </div>
     </div>
   );
 }
