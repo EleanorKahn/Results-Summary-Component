ad756ede84e8b85cdee3f6283a5fcf63a23f1e63
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-04-21 22:42:05 -0700

    further css play

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
         </div>
       </div>
   
-      <div class="attribution">
+      <div className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
         Coded by <a href="#">Eleanor</a>.
       </div>
