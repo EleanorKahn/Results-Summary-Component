ba59ed64f359c04fe9f4b946f691957894dd44c4
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-04-24 17:40:34 -0700

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "darkGrayBlue": "hsl(224, 30%, 27%)",
+        "lightLavender": "hsl(241, 100%, 89%)",
+        "paleBlue": "hsl(221, 100%, 96%)",
+        "lightRed": "hsl(0, 100%, 67%)",
+        "orangeYellow": "hsl(39, 100%, 56%)",
+        "greenTeal": "hsl(166, 100%, 37%)",
+        "cobaltBlue": "hsl(234, 85%, 45%)",
+        "lightSlateBlue": "hsl(252, 100%, 67%)",
+        "lightRoyalBlue": "hsl(241, 81%, 54%)",
+        "violetBlue": "hsla(256, 72%, 46%, 1)",
+        "persianBlue": "hsla(241, 72%, 46%, 0)"
+      }
+    },
+  },
+  plugins: [],
+}
+
